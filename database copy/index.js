1fa6e460e336dc52b98b29ae80cbc3c0b7853de5
Simp-lexx <imlexx007@gmail.com>
2019-10-10 16:50:58 +1000

    init

--- database copy/index.js
+++
@@ -1,9 +0,0 @@
-import Sequelize from 'sequelize';
-import dotenv from 'dotenv';
-import pg from 'pg';
-
-pg.defaults.ssl = true;
-
-dotenv.config();
-
-export default new Sequelize(process.env.DATABASE_URL || 'sqlite:./db.dev.sqlite');
